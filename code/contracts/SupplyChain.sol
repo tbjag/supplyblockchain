62d271f015a85a8cbd266452cba41ac5d3c412c2
dssyk883 <soyeonkwon.0@gmail.com>
2024-03-16 13:05:47 -0700

    Setting up addresses

--- code/contracts/SupplyChain.sol
+++ code/contracts/SupplyChain.sol
@@ -79,6 +79,16 @@ contract SupplyChain is Pharmacy, Manufacturer, Wholesale, Insurer {
         drugRequestCount = 0;
         dcCount = 0;
     }
+
+    function addPHaccounts() public {
+        //accnum = 0
+        // you should be able to see an event emitted: PHAdded(address, accNum)
+        super.addThisAsPH(address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266));
+        //accnum = 1
+        super.addThisAsWD(address(0x70997970C51812dc3A010C7d01b50e0d17dc79C8));
+        //accnum = 2
+        super.addThisAsMA(address(0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC));
+    }
     
     //Adding drug information about drug id, name, and price
     function addDrug(string memory name, uint price) public {
